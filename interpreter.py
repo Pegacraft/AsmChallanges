38bab2e36a64861e190e37945940cd50627ac0c7
Pegacraffft <karl.w.schramm@gmail.com>
2022-01-13 14:56:42 +0100

    Added some computations. #TODO: Program counter

--- interpreter.py
+++ interpreter.py
@@ -1,11 +1,17 @@
 import re
 
+import virtualenv as vm
+
 
 def interpret():
     content: list = read_file("asm-code")
     for line in content:
         if line != "\n":
-            print(tokenize_line(line))
+            tokenized_line: tuple = tokenize_line(line)
+            print(tokenized_line)
+            compute_line(tokenized_line[0], tokenized_line[1])
+            vm.debug_print()
+            vm.program_counter += 1
 
 
 def read_file(file_name: str):
@@ -59,3 +65,173 @@ def tokenize_line(command: str):
                 command = command.replace(re.search(token, command).group(), "", 1)
                 break
     return tokenized_code, number
+
+
+def compute_line(tokenized_line: list, passed_num: int):
+    def comment_remove():
+        vm.program_counter -= 1
+
+    def immediate_assignment():
+        vm.reg_a = passed_num
+
+    # Copy commands
+    def reg_a_copy_reg_d():
+        vm.reg_d = vm.reg_a
+
+    def reg_d_copy_reg_a():
+        vm.reg_a = vm.reg_d
+
+    def reg_d_copy_ram():
+        vm.ram_write(vm.reg_a, vm.reg_d)
+
+    def reg_a_copy_ram():
+        vm.ram_write(vm.reg_a, vm.reg_a)
+
+    def ram_copy_reg_a():
+        vm.reg_a = vm.ram_read(vm.reg_a)
+
+    def ram_copy_reg_d():
+        vm.reg_d = vm.ram_read(vm.reg_a)
+
+    # Plus operation
+    def reg_a_plus():
+        vm.reg_a = vm.reg_a + vm.reg_d
+
+    def reg_d_plus():
+        vm.reg_d = vm.reg_a + vm.reg_d
+
+    # Ram plus
+    def reg_a_ram_plus():
+        vm.reg_a = vm.ram_read(vm.reg_a) + vm.reg_d
+
+    def reg_d_ram_plus():
+        vm.reg_d = vm.ram_read(vm.reg_a) + vm.reg_d
+
+    # Minus operation
+    def reg_d_a_minus_d():
+        vm.reg_d = vm.reg_a - vm.reg_d
+
+    def reg_d_d_minus_a():
+        vm.reg_d = vm.reg_d - vm.reg_a
+
+    def reg_a_a_minus_d():
+        vm.reg_a = vm.reg_a - vm.reg_d
+
+    def reg_a_d_minus_a():
+        vm.reg_a = vm.reg_d - vm.reg_a
+
+    # Minus ram
+    def reg_a_ram_minus_d():
+        vm.reg_a = vm.ram_read(vm.reg_a) - vm.reg_d
+
+    def reg_a_d_minus_ram():
+        vm.reg_a = vm.reg_d - vm.ram_read(vm.reg_a)
+
+    def reg_d_ram_minus_d():
+        vm.reg_d = vm.ram_read(vm.reg_a) - vm.reg_d
+
+    def reg_d_d_minus_ram():
+        vm.reg_d = vm.reg_d - vm.ram_read(vm.reg_a)
+
+    # And operation
+    def reg_a_and():
+        vm.reg_a = vm.reg_a & vm.reg_d
+
+    def reg_d_and():
+        vm.reg_d = vm.reg_a & vm.reg_d
+
+    # Ram and operation
+    def reg_a_ram_and():
+        vm.reg_a = vm.ram_read(vm.reg_a) & vm.reg_d
+
+    def reg_d_ram_and():
+        vm.reg_d = vm.ram_read(vm.reg_a) & vm.reg_d
+
+    # Or operation
+    def reg_a_or():
+        vm.reg_a = vm.reg_a | vm.reg_d
+
+    def reg_d_or():
+        vm.reg_d = vm.reg_a | vm.reg_d
+
+    # Ram or operation
+    def reg_a_ram_or():
+        vm.reg_a = vm.ram_read(vm.reg_a) | vm.reg_d
+
+    def reg_d_ram_or():
+        vm.reg_d = vm.ram_read(vm.reg_a) | vm.reg_d
+
+    if tokenized_line[0] == "COMMENT":
+        comment_remove()
+    # Copy operation
+    if tokenized_line == ["REG_A", "EQUALS", "NUMBER", "END_LINE"]:
+        immediate_assignment()
+    if tokenized_line == ["REG_D", "EQUALS", "REG_A", "END_LINE"]:
+        reg_a_copy_reg_d()
+    if tokenized_line == ["REG_A", "EQUALS", "REG_D", "END_LINE"]:
+        reg_d_copy_reg_a()
+    # Ram copy operation
+    if tokenized_line == ["ASTRIX", "REG_A", "EQUALS", "REG_A", "END_LINE"]:
+        reg_a_copy_ram()
+    if tokenized_line == ["ASTRIX", "REG_A", "EQUALS", "REG_D", "END_LINE"]:
+        reg_d_copy_ram()
+    if tokenized_line == ["REG_A", "EQUALS", "ASTRIX", "REG_A", "END_LINE"]:
+        ram_copy_reg_a()
+    if tokenized_line == ["REG_D", "EQUALS", "ASTRIX", "REG_A", "END_LINE"]:
+        ram_copy_reg_d()
+    # Plus operation
+    if tokenized_line == ["REG_A", "EQUALS", "REG_A", "PLUS", "REG_D", "END_LINE"]:
+        reg_a_plus()
+    if tokenized_line == ["REG_D", "EQUALS", "REG_A", "PLUS", "REG_D", "END_LINE"]:
+        reg_d_plus()
+    if tokenized_line == ["REG_A", "EQUALS", "REG_D", "PLUS", "REG_A", "END_LINE"]:
+        reg_a_plus()
+    if tokenized_line == ["REG_D", "EQUALS", "REG_D", "PLUS", "REG_A", "END_LINE"]:
+        reg_d_plus()
+    # Plus ram operation
+    if tokenized_line == ["REG_A", "EQUALS", "ASTRIX", "REG_A", "PLUS", "REG_D", "END_LINE"]:
+        reg_a_ram_plus()
+    if tokenized_line == ["REG_A", "EQUALS", "REG_D", "PLUS", "ASTRIX", "REG_A", "END_LINE"]:
+        reg_a_ram_plus()
+    if tokenized_line == ["REG_D", "EQUALS", "ASTRIX", "REG_A", "PLUS", "REG_D", "END_LINE"]:
+        reg_d_ram_plus()
+    if tokenized_line == ["REG_D", "EQUALS", "REG_D", "PLUS", "ASTRIX", "REG_A", "END_LINE"]:
+        reg_d_ram_plus()
+    # Minus operation
+    if tokenized_line == ["REG_A", "EQUALS", "REG_A", "MINUS", "REG_D", "END_LINE"]:
+        reg_a_a_minus_d()
+    if tokenized_line == ["REG_D", "EQUALS", "REG_A", "MINUS", "REG_D", "END_LINE"]:
+        reg_d_a_minus_d()
+    if tokenized_line == ["REG_A", "EQUALS", "REG_D", "MINUS", "REG_A", "END_LINE"]:
+        reg_a_d_minus_a()
+    if tokenized_line == ["REG_D", "EQUALS", "REG_D", "MINUS", "REG_A", "END_LINE"]:
+        reg_d_d_minus_a()
+    # Ram minus operation
+    if tokenized_line == ["REG_A", "EQUALS", "ASTRIX", "REG_A", "MINUS", "REG_D", "END_LINE"]:
+        reg_a_ram_minus_d()
+    if tokenized_line == ["REG_D", "EQUALS", "ASTRIX", "REG_A", "MINUS", "REG_D", "END_LINE"]:
+        reg_d_ram_minus_d()
+    if tokenized_line == ["REG_A", "EQUALS", "REG_D", "MINUS", "ASTRIX", "REG_A", "END_LINE"]:
+        reg_a_d_minus_ram()
+    if tokenized_line == ["REG_D", "EQUALS", "REG_D", "MINUS", "ASTRIX", "REG_A", "END_LINE"]:
+        reg_d_d_minus_ram()
+    # And operation
+    if tokenized_line == ["REG_A", "EQUALS", "REG_A", "AND", "REG_D", "END_LINE"]:
+        reg_a_and()
+    if tokenized_line == ["REG_D", "EQUALS", "REG_A", "AND", "REG_D", "END_LINE"]:
+        reg_d_and()
+    # And ram operation
+    if tokenized_line == ["REG_A", "EQUALS", "ASTRIX", "REG_A", "AND", "REG_D", "END_LINE"]:
+        reg_a_ram_and()
+    if tokenized_line == ["REG_D", "EQUALS", "ASTRIX", "REG_A", "AND", "REG_D", "END_LINE"]:
+        reg_d_ram_and()
+    # Or operation
+    if tokenized_line == ["REG_A", "EQUALS", "REG_A", "OR", "REG_D", "END_LINE"]:
+        reg_a_or()
+    if tokenized_line == ["REG_D", "EQUALS", "REG_A", "OR", "REG_D", "END_LINE"]:
+        reg_d_or()
+    # Or ram operation
+    if tokenized_line == ["REG_A", "EQUALS", "ASTRIX", "REG_A", "OR", "REG_D", "END_LINE"]:
+        reg_a_ram_or()
+    if tokenized_line == ["REG_D", "EQUALS", "ASTRIX", "REG_A", "OR", "REG_D", "END_LINE"]:
+        reg_d_ram_or()
--- virtualenv.py
+++ virtualenv.py
@@ -1,6 +1,11 @@
 reg_a: int = 0
 reg_d: int = 0
 ram: dict = {}
+program_counter: int = 0
+
+
+def debug_print():
+    print(f"Reg_A: {reg_a} | Reg_D: {reg_d} | PC: {program_counter} | Memory: {ram}")
 
 
 def save_a(val: int):
@@ -21,6 +26,16 @@ def load_a():
 def load_d():
     global reg_d
     return reg_d
+
+
+def save_pc(val: int):
+    global program_counter
+    program_counter = val
+
+
+def load_pc():
+    global program_counter
+    return program_counter
 
 
 def ram_write(addr: int, val: int):
